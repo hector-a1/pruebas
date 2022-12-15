affdff6271298fe2e83c81303d466f49d3700c7c
hector-a1 <104702707+hector-a1@users.noreply.github.com>
2022-12-15 11:26:15 -0600

    Update holamundo.c

--- holamundo.c
+++ holamundo.c
@@ -1,5 +1,14 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class holamundo{
+  public List<GameObject> botones;
+  public GameObject caja;
+  
   Debug.Log("holamundo");
+  Caja=GameObject.FindWithTag("global");
+  
 }
